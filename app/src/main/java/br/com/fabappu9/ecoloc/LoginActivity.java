0b3bbfec3ffa666045885a682f62ad3d907e58a5
VinnyFranca <vinifrancasilva@gmail.com>
2017-10-28 09:04:28 -0200

    Arquivos que faltavam, acitivity login, layout login, drwable login btn

---
+++ app/src/main/java/br/com/fabappu9/ecoloc/LoginActivity.java
@@ -0,0 +1,50 @@
+package br.com.fabappu9.ecoloc;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+
+    Button cadastrar;
+    Button login;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        cadastrar  = (Button) findViewById(R.id.CadastrarButton);
+
+        cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(LoginActivity.this, CadastradoActivity.class);
+                startActivity(intent1);
+
+            }
+        });
+
+
+        login  = (Button) findViewById(R.id.LoginButton);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent1);
+
+            }
+        });
+    }
+
+
+
+
+}
