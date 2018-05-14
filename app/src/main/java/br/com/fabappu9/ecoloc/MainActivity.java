afa05bab94ff852b0aa70fd0dc71e9dc2603c631
Andre Vieira Goncalves <andrevieira@brq.com>
2018-05-14 10:54:34 -0300

    deslogar pronto

--- app/src/main/java/br/com/fabappu9/ecoloc/MainActivity.java
+++ app/src/main/java/br/com/fabappu9/ecoloc/MainActivity.java
@@ -1,39 +1,74 @@
 package br.com.fabappu9.ecoloc;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
 
+import br.com.fabappu9.ecoloc.data.SharedPreferenceHelper;
+
 
 public class MainActivity extends AppCompatActivity {
-   @Override
+
+    Toolbar toolbar;
+    SharedPreferenceHelper sharedPreferences;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-       bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
-           @Override
-           public void onTabSelected(@IdRes int tabId) {
+        sharedPreferences = new SharedPreferenceHelper(MainActivity.this);
+        
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
 
-               switch (tabId){
-                   case R.id.mapa:
-                       getFragmentManager().beginTransaction().replace(R.id.container, new MapaFragment(),"mapa").commit();
-                   break;
-                   case R.id.perfil:
-                       getFragmentManager().beginTransaction().replace(R.id.container, new PerfilFragment(),"perfil").commit();
-                   break;
-               }
+                switch (tabId) {
+                    case R.id.mapa:
+                        getFragmentManager().beginTransaction().replace(R.id.container, new MapaFragment(), "mapa").commit();
+                        break;
+                    case R.id.perfil:
+                        getFragmentManager().beginTransaction().replace(R.id.container, new PerfilFragment(), "perfil").commit();
+                        break;
+                }
 
-           }
-       });
+            }
+        });
 
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.itemSair:
+                sharedPreferences.setCheckLogin();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
 
 
